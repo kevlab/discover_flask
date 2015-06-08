7055a65f693b17953d72501c73306fdb05d256a4
Kevin Labtani <greenleaf1348@gmail.com>
2015-06-08 16:50:23 +0200

    add sql database

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, redirect, url_for, request, session,\
-                  flash
+                  flash, g
 from functools import wraps
+import sqlite3
 
 app = Flask(__name__)
 
 app.secret_key = "my secret"  # replace
+app.database = "sample.db"
 
 
 def login_required(f):
@@ -21,7 +23,12 @@ def login_required(f):
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.html')
+    # g to store temporary data for the duration of a request
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1])for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/welcome')
@@ -49,6 +56,10 @@ def logout():
     session.pop('logged_in', None)
     flash('You were just logged out')
     return redirect(url_for('welcome'))
+
+
+def connect_db():
+    return sqlite3.connect(app.database)
 
 if __name__ == '__main__':
     app.run(debug=True)
