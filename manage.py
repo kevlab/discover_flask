55da5a04968f19eef41a6ec58016fef79a9fe0fd
Kevin Labtani <greenleaf1348@gmail.com>
2015-06-09 21:25:35 +0200

    add migration comment

--- manage.py
+++ manage.py
@@ -12,3 +12,12 @@ manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
+
+# to run migration:
+# $ python app.py db init nb:only 1st time we make a migration
+# update our models.py
+# $ python app.py db migrate
+# $ python app.py db upgrade
+
+# to update heroku, push migrations folder then
+# heroku run python manage.py db upgrade
