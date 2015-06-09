f40348c0ac9e17acee20792062c55abc21622f8e
Kevin Labtani <greenleaf1348@gmail.com>
2015-06-09 14:22:56 +0200

    secret key stuff

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 import os
 
 # don't push config to git; just an example here
+# use os.urandom(24) to generate a key and store key in environment var
+# alt to load from file:
+# with open('/etc/secret_key.txt') as f:
+#     SECRET_KEY = f.read().strip()
+
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = "a secret"
+    SECRET_KEY = 'o\xc6dG\x1eP\x04E\xd1\xe9\xc8\x8b\xbb\x06\x0e\x92\xcd\xfbn\xdb\xb1\xdf\xda\xaa'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
