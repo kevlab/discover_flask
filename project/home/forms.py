26422672b9dbc4b689823cc69329a19a64e75715
Kevin Labtani <greenleaf1348@gmail.com>
2015-06-11 08:10:19 +0200

    add messaging system on home allowing users to add posts

---
+++ project/home/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+
+class MessageForm(Form):
+    title = StringField('Title', validators=[DataRequired()])
+    description = StringField('Description',
+                             validators=[DataRequired(), Length(max=150)])
--- project/home/views.py
+++ project/home/views.py
@@ -1,18 +1,33 @@
 from project import db
 from project.models import BlogPost
-from flask import render_template, Blueprint
-from flask.ext.login import login_required
+from flask import render_template, Blueprint, redirect, url_for, request, flash
+from flask.ext.login import login_required, current_user
+from forms import MessageForm
 
 home_blueprint = Blueprint(
     'home', __name__,
     template_folder='templates')
 
 
-@home_blueprint.route('/')
+@home_blueprint.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    posts = db.session.query(BlogPost).all()
-    return render_template('index.html', posts=posts)
+    error = None
+    form = MessageForm(request.form)
+    if form.validate_on_submit():
+        new_message = BlogPost(
+            form.title.data,
+            form.description.data,
+            current_user.id
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        flash('New entry was successfully posted. Thanks.')
+        return redirect(url_for('home.home'))
+    else:
+        posts = db.session.query(BlogPost).all()
+        return render_template(
+            'index.html', posts=posts, form=form, error=error)
 
 
 @home_blueprint.route('/welcome')
